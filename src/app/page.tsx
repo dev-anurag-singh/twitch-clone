200fdc7841c2fcc6f077a931577ba87eee336245
Anurag <dev.anuragkr@gmail.com>
2024-01-02 23:48:14 +0530

    removed title from logo

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1 className='text-red-500 text-xl'>
-        Only Authenticated user might access this page
-      </h1>
-    </div>
-  );
-}
