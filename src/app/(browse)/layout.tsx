01906e7f3196c67713e846442c77d9b1f8902a53
Anurag <dev.anuragkr@gmail.com>
2024-01-04 23:12:46 +0530

    added loading state using suspense

--- src/app/(browse)/layout.tsx
+++ src/app/(browse)/layout.tsx
@@ -1,11 +1,18 @@
 import { Navbar } from '@/components/Navbar';
+import Sidebar, { SidebarSkeleton } from '@/components/Sidebar';
+import { Suspense } from 'react';
 
 function BrowseLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className='grid h-full grid-rows-[auto_1fr]'>
         <Navbar />
-        {children}
+        <div className='flex'>
+          <Suspense fallback={<SidebarSkeleton />}>
+            <Sidebar />
+          </Suspense>
+          <div className='grow'>{children}</div>
+        </div>
       </div>
     </>
   );
