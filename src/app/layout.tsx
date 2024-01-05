3199db53385d940f92f29405f9259a0d34610906
Anurag <dev.anuragkr@gmail.com>
2024-01-05 19:29:52 +0530

    added sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { cn } from '@/lib/utils';
 import { dark } from '@clerk/themes';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
             },
           }}
         >
+          <Toaster theme='light' position='top-center' />
           {children}
         </ClerkProvider>
       </body>
