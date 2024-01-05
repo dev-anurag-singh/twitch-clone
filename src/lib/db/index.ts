b21221b7b572891272d9664d3275ce48660190ce
Anurag <dev.anuragkr@gmail.com>
2024-01-05 19:33:30 +0530

    changed export name

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -8,8 +8,8 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const db = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma;
+export default db;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
