9e80727948b08ba7b5da1b1e305ab6f0c445f6ee
Anurag <dev.anuragkr@gmail.com>
2024-01-08 23:44:16 +0530

    keys page

---
+++ src/app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,29 @@
+import ConnectModal from '@/components/keys/connect-modal';
+import KeyCard from '@/components/keys/key-card';
+import UrlCard from '@/components/keys/url-card';
+import { getSelf } from '@/lib/services/auth-service';
+import { getStreamByUserId } from '@/lib/services/stream-service';
+
+async function KeysPage() {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error('Stream not found');
+  }
+
+  return (
+    <div className='p-6'>
+      <div className='flex items-center justify-between mb-4'>
+        <h1 className='text-2xl font-bold'>Keys & URLs</h1>
+        <ConnectModal />
+      </div>
+      <div className='space-y-4'>
+        <UrlCard value={stream.serverUrl} />
+        <KeyCard value={stream.serverUrl} />
+      </div>
+    </div>
+  );
+}
+
+export default KeysPage;
