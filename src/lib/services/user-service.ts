ae4b06d7e759d381dae2fe53dd102252dafc4868
Anurag <dev.anuragkr@gmail.com>
2024-01-10 23:58:58 +0530

    added service to find user by id

--- src/lib/services/user-service.ts
+++ src/lib/services/user-service.ts
@@ -4,6 +4,19 @@ export const getUserByUsername = async (username: string) => {
     where: {
       username,
     },
+    include: {
+      stream: true,
+    },
+  });
+
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
   });
 
   return user;
