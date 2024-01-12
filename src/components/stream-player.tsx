c4ce4df5fddf7d7e84f5e700f52b346300b360e8
Anurag <dev.anuragkr@gmail.com>
2024-01-12 22:45:06 +0530

    created stream player component

---
+++ src/components/stream-player.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useViewerToken } from '@/hooks/use-viewer-token';
+import { Stream, User } from '@prisma/client';
+
+interface StreamPlayerProps {
+  user: User & { stream: Stream | null };
+  stream: Stream;
+  isFollowing: boolean;
+}
+function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
+  const { token, name, identity } = useViewerToken(user.id);
+
+  if (!token || !name || !identity) {
+    return <div>Cannot watch the stream</div>;
+  }
+
+  return <div> Allowed to watch the stream</div>;
+}
+
+export default StreamPlayer;
