b6bb025f3a9b6d0f598003dc7e71d6a22c16248e
Anurag <dev.anuragkr@gmail.com>
2024-01-12 22:45:55 +0530

    creator dashboard

--- src/app/(dashboard)/u/[username]/page.tsx
+++ src/app/(dashboard)/u/[username]/page.tsx
@@ -1,5 +1,26 @@
-function CreatorDashboardPage() {
-  return <div>Creator Dashboard</div>;
+import StreamPlayer from '@/components/stream-player';
+import { getUserByUsername } from '@/lib/services/user-service';
+import { currentUser } from '@clerk/nextjs';
+
+interface CreatorDashboardProps {
+  params: { username: string };
+}
+
+async function CreatorDashboardPage({
+  params: { username },
+}: CreatorDashboardProps) {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error('Unauthorized');
+  }
+
+  return (
+    <div className='h-full'>
+      <StreamPlayer user={user} isFollowing stream={user.stream} />
+    </div>
+  );
 }
 
 export default CreatorDashboardPage;
