af6196e42f74fd55dbac317c864dd8dfb6430a21
Anurag <dev.anuragkr@gmail.com>
2024-01-12 22:44:41 +0530

    added hook to create a viewer token

---
+++ src/hooks/use-viewer-token.ts
@@ -0,0 +1,37 @@
+import { toast } from 'sonner';
+import { useEffect, useState } from 'react';
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { createViewerToken } from '@/actions/token';
+
+export const useViewerToken = (hostIdentity: string) => {
+  const [token, setToken] = useState('');
+  const [name, setName] = useState('');
+  const [identity, setIdentity] = useState('');
+
+  useEffect(() => {
+    const createToken = async () => {
+      try {
+        const viewerToken = await createViewerToken(hostIdentity);
+        setToken(viewerToken);
+        const decodedToken = jwtDecode(viewerToken) as JwtPayload & {
+          name?: string;
+        };
+        const name = decodedToken?.name;
+        const identity = decodedToken.jti;
+
+        if (name) {
+          setName(name);
+        }
+        if (identity) {
+          setIdentity(identity);
+        }
+      } catch {
+        toast.error('Something went wrong');
+      }
+    };
+
+    createToken();
+  }, [hostIdentity]);
+
+  return { token, name, identity };
+};
