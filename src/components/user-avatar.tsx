36e7d0d0b480cbc6e6af0be55d9b134b3bf1869d
Anurag <dev.anuragkr@gmail.com>
2024-01-16 23:53:06 +0530

    removed empty space

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,5 +1,4 @@
 import { cva, type VariantProps } from 'class-variance-authority';
-
 import { cn } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
