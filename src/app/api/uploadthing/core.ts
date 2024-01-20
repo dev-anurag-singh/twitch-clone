cc28debb973dc30c4072d8bb25c2f1d1a12a07bc
Anurag <dev.anuragkr@gmail.com>
2024-01-20 20:19:46 +0530

    uploadthings setup

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+import db from '@/lib/db';
+import { getSelf } from '@/lib/services/auth-service';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+
+const f = createUploadthing();
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  thumbnailUploader: f({
+    image: {
+      maxFileCount: 1,
+      maxFileSize: '4MB',
+    },
+  })
+    .middleware(async () => {
+      const self = await getSelf();
+      return { user: self };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db.stream.update({
+        where: {
+          userId: metadata.user.id,
+        },
+        data: {
+          thumbnailUrl: file.url,
+        },
+      });
+      return { fileUrl: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
