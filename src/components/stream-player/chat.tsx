5103b92467c0df266b47b5f252c172065c4bf2b0
Anurag <dev.anuragkr@gmail.com>
2024-01-21 17:11:20 +0530

    removed border

--- src/components/stream-player/chat.tsx
+++ src/components/stream-player/chat.tsx
@@ -65,7 +65,7 @@ export const Chat = ({
   };
 
   return (
-    <div className='flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]'>
+    <div className='flex flex-col bg-background border-b pt-0 h-[calc(100vh-80px)]'>
       <ChatHeader />
       {variant === ChatVariant.CHAT && (
         <>
